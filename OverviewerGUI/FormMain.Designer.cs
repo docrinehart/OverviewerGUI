9520a6ee320b8823cd4867ca6c40c853b8d03971
sean-rinehart <sean.rinehart@reaperdev.com>
2014-01-06 20:58:52 -0600

    Initial Upload
    Version 0.1 of the GUI;
    Added text-boxes for Source, Output, & Config directories;
    Added default text to source box for easy SP maps;

---
+++ OverviewerGUI/FormMain.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace OverviewerGUI
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSourceDir = new System.Windows.Forms.TextBox();
+            this.txtOutputDir = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtConfigDir = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Output Directory";
+            // 
+            // txtSourceDir
+            // 
+            this.txtSourceDir.Location = new System.Drawing.Point(108, 38);
+            this.txtSourceDir.Name = "txtSourceDir";
+            this.txtSourceDir.Size = new System.Drawing.Size(440, 20);
+            this.txtSourceDir.TabIndex = 2;
+            this.txtSourceDir.Text = "%appdata%\\.minecraft\\saves\\<single-player-world-name>";
+            // 
+            // txtOutputDir
+            // 
+            this.txtOutputDir.Location = new System.Drawing.Point(108, 64);
+            this.txtOutputDir.Name = "txtOutputDir";
+            this.txtOutputDir.Size = new System.Drawing.Size(440, 20);
+            this.txtOutputDir.TabIndex = 3;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(445, 174);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(103, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Start Rendering";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(340, 174);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(99, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close GUI";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtConfigDir
+            // 
+            this.txtConfigDir.Location = new System.Drawing.Point(108, 90);
+            this.txtConfigDir.Name = "txtConfigDir";
+            this.txtConfigDir.Size = new System.Drawing.Size(440, 20);
+            this.txtConfigDir.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Config File";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 209);
+            this.Controls.Add(this.txtConfigDir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtOutputDir);
+            this.Controls.Add(this.txtSourceDir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.Text = "Overviewer GUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSourceDir;
+        private System.Windows.Forms.TextBox txtOutputDir;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtConfigDir;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ OverviewerGUI/FormMain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OverviewerGUI
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            /*
+             * overviewer.exe [--rendermodes=...] [options] <World> <Output Dir>
+             * overviewer.exe --config=<config file> [options]
+            */
+
+            string source = txtSourceDir.Text;
+            string output = txtOutputDir.Text;
+            string config = txtConfigDir.Text;
+            string oArgs = "";
+
+            if (!String.IsNullOrWhiteSpace(config))
+            {
+                oArgs = "--config=\"" + config + "\"";
+            }
+            else
+            {
+                oArgs = "" + "\"" + source + "\" \"" + output + "\"";
+            }
+            // TODO: oArgs += [options]
+
+            // Open console and run command
+            try
+            {
+                System.Diagnostics.Process.Start("overviewer.exe", oArgs);
+                //this.Close();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                label1.Text = "File Not Found";
+            }
+
+        }
+    }
+}
